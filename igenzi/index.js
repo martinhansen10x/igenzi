f5b6b42767efd455726127da7fb7fa082c7eec11
Martin Hansen <martin.hansen@10xbanking.com>
2020-07-26 00:14:40 +0100

    add demo video

--- igenzi/index.js
+++ igenzi/index.js
@@ -7,4 +7,5 @@ import 'react-native-gesture-handler'
 import App from './src/App'
 import {name as appName} from './app.json'
 
+console.disableYellowBox = true
 AppRegistry.registerComponent(appName, () => App)
